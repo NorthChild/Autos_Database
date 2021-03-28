b52e910c310f7be6af8f977186d1f0709fd1c118
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-03-28 10:44:42 +0200

    Update pdo.php

--- Autos Database/pdo.php
+++ Autos Database/pdo.php
@@ -2,7 +2,7 @@
 <?php
 
 // this file is in a separate file from the actual file that required the variable $pdo, this is the connection to the server, and it will be saved for this example in a separate file called 'pdo.php'
-$pdo = new PDO ('mysql:host=localhost;port=8888;dbname=misc','fred','zap');
+$pdo = new PDO ('mysql:host=localhost;port=8888;dbname=misc','name','password');
 
 $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
