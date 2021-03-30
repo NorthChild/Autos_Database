ed07c61b8efe7dcd4da2c3610f7e77e9067066a0
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-03-30 16:05:00 +0200

    Update logout.php

--- Autos Database/logout.php
+++ Autos Database/logout.php
@@ -5,3 +5,14 @@ session_destroy();
 header("Location: index.php");
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Michael John Carini - Autos DB CRUD</title>
+  </head>
+  <body>
+
+  </body>
+</html>
